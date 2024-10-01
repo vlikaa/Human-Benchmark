6c76a1b19dc8155b40663d46a788f0ed40f6062f
vlikaa <149687088+vlikaa@users.noreply.github.com>
2024-10-01 16:26:31 +0400

    added Timer & Random

--- Human-Benchmark/ViewModels/RedViewModel.cs
+++ Human-Benchmark/ViewModels/RedViewModel.cs
@@ -1,6 +1,33 @@
-﻿namespace Human_Benchmark.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+using Human_Benchmark.Messages;
+using Human_Benchmark.Services;
 
-public class RedViewModel : BaseViewModel
+namespace Human_Benchmark.ViewModels;
+
+[INotifyPropertyChanged]
+public partial class RedViewModel : BaseViewModel
 {
+	public RedViewModel(ViewModelFactory factory)
+	{
+		WeakReferenceMessenger.Default.Register<StartTimerMessage>(this,
+			(_, _) =>
+			{
+				_timer.Interval = _random.Next(1500, 5000);
+				_timer.Elapsed += (_, _) =>
+				{
+					WeakReferenceMessenger.Default
+						.Send(new ChangeViewModelMessage(factory.Create(typeof(GreenViewModel))));
+				};
+				
+				_timer.Start();
+			});
+	}
+	
+	private readonly Random _random = new();
+	private readonly System.Timers.Timer _timer = new()
+	{
+		AutoReset = false
+	};
 	
 }
