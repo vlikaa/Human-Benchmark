f31d3123ce46edbdf8ab8dd01996facb4f5ae19a
vlikaa <149687088+vlikaa@users.noreply.github.com>
2024-10-01 18:50:39 +0400

    added ResultViewModel

---
+++ Human-Benchmark/ViewModels/ResultViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Human_Benchmark.Messages;
+using Human_Benchmark.Services;
+
+namespace Human_Benchmark.ViewModels;
+
+[INotifyPropertyChanged]
+public partial class ResultViewModel(ViewModelFactory factory) : BaseViewModel
+{
+	[RelayCommand]
+	private void OpenRedView()
+	{
+		WeakReferenceMessenger.Default
+			.Send(new ChangeViewModelMessage(factory.Create(typeof(RedViewModel))));
+		
+		WeakReferenceMessenger.Default
+			.Send(new StartTimerMessage());
+	}
+}
