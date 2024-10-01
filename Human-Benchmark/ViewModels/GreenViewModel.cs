58ef9d5f4bd98e193222c0ea48c54a5a54797685
vlikaa <149687088+vlikaa@users.noreply.github.com>
2024-10-01 18:50:05 +0400

    added command

--- Human-Benchmark/ViewModels/GreenViewModel.cs
+++ Human-Benchmark/ViewModels/GreenViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace Human_Benchmark.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Human_Benchmark.Messages;
+using Human_Benchmark.Services;
 
-public class GreenViewModel : BaseViewModel
+namespace Human_Benchmark.ViewModels;
+
+[INotifyPropertyChanged]
+public partial class GreenViewModel(ViewModelFactory factory) : BaseViewModel
 {
-	
+	[RelayCommand]
+	private void StopTimer()
+	{
+		WeakReferenceMessenger.Default.Send(new StopTimerMessage());
+
+		WeakReferenceMessenger.Default.Send(new ChangeViewModelMessage(factory.Create(typeof(ResultViewModel))));
+	}
 }
