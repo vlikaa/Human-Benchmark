9137a64b5241ac1589de90d125613e3e02b8e861
vlikaa <149687088+vlikaa@users.noreply.github.com>
2024-10-02 00:41:47 +0400

    added PassIntervalMessage & StartPeriodMessage

---
+++ Human-Benchmark/Messages/PassIntervalMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace Human_Benchmark.Messages;
+
+public class PassIntervalMessage(string intervalString) : IMessage
+{
+	public string IntervalString { get; } = intervalString;
+}
---
+++ Human-Benchmark/Messages/StartPeriodMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace Human_Benchmark.Messages;
+
+public class StartPeriodMessage : IMessage
+{
+	public DateTime StartPeriodTime { get; } = DateTime.Now;
+}
