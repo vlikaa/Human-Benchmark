a3a67220c3f0c8d1b4eeb77cbf05570d64835e2b
vlikaa <149687088+vlikaa@users.noreply.github.com>
2024-11-08 16:13:05 +0400

    added TooSoon view

---
+++ Human-Benchmark/ViewModels/TooSoonViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Human_Benchmark.Messages;
+using Human_Benchmark.Services;
+
+namespace Human_Benchmark.ViewModels;
+
+public partial class TooSoonViewModel(ViewModelFactory factory) : BaseViewModel
+{
+	[RelayCommand]
+	private void OpenRedView()
+	{
+		WeakReferenceMessenger.Default
+			.Send(new ChangeViewModelMessage(factory.Create(typeof(RedViewModel))));
+		
+		WeakReferenceMessenger.Default
+			.Send(new StartTimerMessage());
+	}
+}
